c005b7e5ff04eda3f292d16faabe84ad2721a8ce
Kate Buxton <k8hunter@hotmail.com>
2019-01-18 16:39:57 +0000

    added navbar and banner

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,17 @@
 //= require rails-ujs
 //= require_tree .
+
+
+// transparent to coloured navbar
+
+$(document).ready(function() {
+        // Transition effect for navbar
+        $(window).scroll(function() {
+          // checks if window is scrolled more than 20px, adds/removes solid class
+          if($(this).scrollTop() > 20) {
+              $('.navbar-wagon').addClass('solid');
+          } else {
+              $('.navbar-wagon').removeClass('solid');
+          }
+        });
+});
