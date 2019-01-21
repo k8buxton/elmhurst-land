9621b4ecee1149c9ad8c1d0416bca31dc55b061f
Kate Buxton <k8hunter@hotmail.com>
2019-01-21 09:57:36 +0000

    transparent to coloured navbar added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,16 +2,16 @@
 //= require_tree .
 
 
-// transparent to coloured navbar
+// transparent to coloured navbar https://codepen.io/sonorangirl/pen/XmRBjq
 
-$(document).ready(function() {
-        // Transition effect for navbar
-        $(window).scroll(function() {
-          // checks if window is scrolled more than 20px, adds/removes solid class
-          if($(this).scrollTop() > 20) {
-              $('.navbar-wagon').addClass('solid');
-          } else {
-              $('.navbar-wagon').removeClass('solid');
-          }
-        });
-});
+// $(document).ready(function() {
+//         // Transition effect for navbar
+//         $(window).scroll(function() {
+//           // checks if window is scrolled more than 20px, adds/removes solid class
+//           if($(this).scrollTop() > 20) {
+//               $('.navbar-wagon').addClass('solid');
+//           } else {
+//               $('.navbar-wagon').removeClass('solid');
+//           }
+//         });
+// });
